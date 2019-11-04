4244f9bb46cd2555a6b4eea381db8621f8348fc8
domin <dominika.gothub@gmail.com>
2019-11-04 14:06:37 +0100

    najprostrze rozwiązanie

--- Cezar.py
+++ Cezar.py
@@ -1,40 +1,10 @@
-
-def cezar():
-    alfabet = ['A']
-    zakodowane = []
-    literySlowa = []
-    key = 2
-    pomocnicza = 0
-    for i in range(0, 25):
-        pomocnicza = ord(alfabet[i]) + 1
-        alfabet.append(chr(pomocnicza))
-
-    for i in range(0, 25):
-        pomocnicza = ord(alfabet[i]) + key
-        zakodowane.append(chr(pomocnicza))
-
-    if ord(zakodowane[24]) > 90:
-        zakodowane.remove('[')
-        zakodowane.append(chr(65))
-        zakodowane.append(chr(66))
+import string
+def cezar(napis: str, klucz: int):
+    alfabet = string.ascii_uppercase
+    kod = alfabet[klucz:] + alfabet[:klucz]
+    tabela = str.maketrans(alfabet, kod)
+    return napis.translate(tabela)
 
 
-
-    while(True):
-        slowoDoKod = input('Podaj slowo do zakodowania, '
-                           'wpisuj pojedyncze wielkie litery'
-                           'żeby zakonczyc wpisywanie wpisz END: ')
-        if slowoDoKod == 'END':
-            break
-        else:
-            literySlowa.append(slowoDoKod)
-
-    print(literySlowa)
-
-
-
-
-
-
-
-cezar()
+print(cezar('PYTHON', 2))
+print(cezar('RAVJQP', -2))
